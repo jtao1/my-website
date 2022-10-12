af267700111b5c13ca54f1664e9538b52e73ca95
Junshen <tao122@purdue.edu>
2022-10-12 17:17:41 -0400

    updated projects

--- src/HomePage/Sections/ProjectsSection..js
+++ src/HomePage/Sections/ProjectsSection..js
@@ -6,6 +6,12 @@ export default function ProjectsSection() {
         <div className='projects'>
             <h1 className='projects_title'>Projects</h1>
             <div className='projects_contents'>
+                <h1 className='projects_project_title'><a className='projects_links' href='https://jtao1-stocks-visualization-stocks-display-roe5sa.streamlitapp.com/'>Stock Visualization</a></h1>
+                <ul className='projects_project_info'>
+                    <li>Displayed multiple stock charts with technical indicators simultaneously based on stock input</li>
+                    <li>Gave users customization options with line colors and indicator length</li>
+                    <li>Built with Python (matplotlib, pandas, numpy, streamlit), utilized google cloud storage</li>
+                </ul>    
                 <h1 className='projects_project_title'>Personal Website</h1>
                 <ul>
                     <li>Built with React, designed on Figma, utilized React Router, deployed on Heroku</li>
@@ -14,7 +20,7 @@ export default function ProjectsSection() {
                 <ul className='projects_project_info'>
                     <li>Utilized Java: file i/o, sockets, swing</li>
                     <li>Worked with others to divide the project equally and set biweekly goals</li>
-                </ul>    
+                </ul>
             </div>
         </div>
     )
