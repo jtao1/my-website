3d12e3652847168d763c3e1036a4cf7a08400791
Junshen <tao122@purdue.edu>
2023-05-21 17:18:32 -0500

    updated top and bot nav

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom'
 
 export default function TopNavBar() {
     return (
-        <nav className='top_nav_bar'>
-            <Link className='top_nav_bar_home' to='/'>
-                <h1 className='top_nav_bar_text' >Home.</h1>
-            </Link>
-            <Link className='top_nav_bar_photography' to='/photography'>
-                <h1 className='top_nav_bar_text' >Photography.</h1>
-            </Link>
+        <nav className='top_nav_bar'>            
+            <div className='top_nav_bar_left_nav'>
+                <Link className='top_nav_bar_home' to='/'>
+                    <h1 className='top_nav_bar_text' >Home.</h1>
+                </Link>
+            </div>
+            <div className='top_nav_bar_center'>
+                <img className='top_nav_bar_signature' src={require('./signature.png')} alt='signature'/>
+            </div>
+            <div className='top_nav_bar_right_nav'>
+                <Link className='top_nav_bar_photography' to='/photography'>
+                    <h1 className='top_nav_bar_text'>Photography.</h1>
+                </Link>
+            </div>
             {/*
             <Link className='top_nav_bar_blog' to='/blog'>
                 <h1 className='top_nav_bar_text'>Blog.</h1>
