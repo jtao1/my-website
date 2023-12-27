f3102986370bacbe712a23748dde72ef7d70b35d
Junshen <tao122@purdue.edu>
2023-12-27 17:14:50 -0600

    added descriptions to projects

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -23,31 +23,39 @@ export default function HomePage() {
         {
             name: 'IdolDB',
             image: defaultbg,
-            description: 'Description for IdolDB project.',
+            description: `This was my final project for my databases class where I used Holoviz Panel and Google Cloud's MYSQL.
+                It allowed the user to interact with the database and provided a summary statistics of the KPOP idols.`,
             link: 'https://github.com/jtao1/idoldb',
         },
         {
             name: 'AI Children Stories',
             image: ai_childrenstories,
-            description: 'Description for AI Children Stories project.',
+            description: `This app was made for Anaconda's 2023 data app contest, where it won second place and was my introduction to using Holoviz Panel. 
+                I incorporated OpenAI's ChatGPT and DALL-E to allow for text and image generation based on user-created prompts.`,
             link: 'https://github.com/jtao1/ai_children_stories',
         },
         {
             name: 'Garage Sales Tracker',
             image: garage_sales_tracker,
-            description: 'Description for Garage Sales Tracker project.',
+            description: `In the summer of 2023, I attempted to flip items from garage sales. 
+                One issue that I came across was that there were multiple sites for sites tracking garage sales, 
+                which made it time-consuming to mark out where I wanted to go for the day; this was my motivation for creating this app.
+                I aggregated the data from 3 different websites and displayed them all using Folium and Panel`,
             link: 'https://github.com/jtao1/garage-sales-tracker',
         },
         {
             name: 'Stocks Chart',
             image: stocks_chart,
-            description: 'Description for Stocks Chart project.',
+            description: `This was one of the first personal projects I ever made, 
+                which I began on the summer after freshman year. 
+                I learned to use Python along with its packages including Pandas, Matplotlib, and Streamlit.`,
             link: 'https://github.com/jtao1/stocks-visualization',
         },
         {
             name: 'Personal Website',
             image: website_pic,
-            description: 'Description for Personal Website project.',
+            description: `This is the website you are looking at right now! 
+                This project has taught me how to use ReactJS, HTML, and CSS while pushing me creatively.`,
             link: 'https://github.com/jtao1/my-website',
         },
     ];
